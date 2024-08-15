74fb258e1299789447d351f3a21479b1c68154fe
Dorramires <math.dorramires@gmail.com>
2024-08-15 00:49:00 +0000

    codigos da aula 1

--- aulas/aula1/praticas/pratica01/ola_mundo.c
+++ aulas/aula1/praticas/pratica01/ola_mundo.c
@@ -3,4 +3,4 @@
 int main() {
   printf("Olá, Mundo!\n");
 return 0;
-}
+}   
---
+++ aulas/aula1/sintaxe_c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  //1. ingredientes
+  int primeiro_numero;
+  int segundo_numero;
+  primeiro_numero = 3;
+  segundo_numero = 3;
+  //2. processamento
+  int soma = primeiro_numero + segundo_numero;
+  //3. saida
+  printf("A soma de %i com %i eh %i\n", primeiro_numero, segundo_numero, soma);
+return 0;
+}   
