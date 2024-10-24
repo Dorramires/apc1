ea8f565b6414aa35ae2109541338aa69c41b4e00
Dorramires <math.dorramires@gmail.com>
2024-10-23 21:56:31 -0300

    Update jogo_da_velha.c

--- aulas/jogo_da_velha.c
+++ aulas/jogo_da_velha.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(){
     char tabuleiro [5][5] = {
@@ -10,11 +10,21 @@ int main(){
         {' ','|',' ','|',' '},
     };
     char move[3];
+    char varMove;
     int dest_x, dest_y;
+    int turno = 1;
+    char quem;
 
     while(1){
 
         int i, j;
+
+        // Exibir de quem é a vez
+        if (turno == 1) {
+            printf("Vez do x!\n");
+        } else {
+            printf("Vez do o!\n");
+        }
 
         printf("  a   b   c \n");
         for (i = 0; i < 5; i++) {
@@ -98,9 +108,77 @@ int main(){
                 printf("Coordenadas invalidas, por favor insira novamente!l2\n");
                 continue;
             }
-            // //teste
-            // printf("%i,", dest_x);
-            // printf(" %i\n", dest_y);
+
+            //validar movimento e alocar na matrix
+            varMove = tabuleiro[dest_x][dest_y];
+            if(varMove != ' '){
+                system("clear");
+                printf("Movimento invalido! Jogue novamente.\n");
+            }else{
+                if(turno == 1){
+                    tabuleiro[dest_x][dest_y] = 'x';
+                    //teste
+                    tabuleiro[0][0] = 'x';
+                    tabuleiro[2][2] = 'x';
+                    tabuleiro[4][4] = 'x';
+                    //Alguem ganhou?
+                    switch (turno)
+                    {
+                    case 1:quem = 'x';break;
+                    
+                    default:quem = 'o';
+                    }
+                    if((tabuleiro[0][0] == tabuleiro[2][2] == tabuleiro[4][4]) || (tabuleiro[0][4] == tabuleiro[2][2] && tabuleiro[2][2] == tabuleiro[4][0])){
+                        system("clear");
+                        printf("TEMOS UM VENCENDOR!\n\n");
+                        printf("O jogador %s venceu o jogo!\n\n", quem);
+                        printf("deu certo");
+                        move[0] = 'q';
+                    }else if ((tabuleiro[0][0] == tabuleiro[2][0] == tabuleiro[4][0]) || (tabuleiro[0][2] == tabuleiro[2][2] == tabuleiro[4][2]) || (tabuleiro[0][4] == tabuleiro[2][4] == tabuleiro[4][4])){
+                        system("clear");
+                        printf("TEMOS UM VENCENDOR!\n\n");
+                        printf("O jogador %s venceu o jogo!\n\n", quem);
+                        move[0] = 'q';
+                    }else if((tabuleiro[0][0] == tabuleiro[0][2] == tabuleiro[0][4]) || (tabuleiro[2][0] == tabuleiro[2][2] == tabuleiro[2][4]) || (tabuleiro[4][0] == tabuleiro[4][2] == tabuleiro[4][4])){
+                        system("clear");
+                        printf("TEMOS UM VENCENDOR!\n\n");
+                        printf("O jogador %s venceu o jogo!\n\n", quem);
+                        move[0] = 'q';
+                    }
+                    --turno;
+                    system("clear");
+                }else{
+                    tabuleiro[dest_x][dest_y] = 'o';
+                    //Alguem ganhou?
+                    switch (turno)
+                    {
+                    case 1:quem = 'x';break;
+                    
+                    default:quem = 'o';
+                    }
+                    if((tabuleiro[0][0] && tabuleiro[2][2] && tabuleiro[4][4]) || (tabuleiro[0][4] && tabuleiro[2][2] && tabuleiro[4][0])== quem){
+                        system("clear");
+                        printf("TEMOS UM VENCENDOR!\n\n");
+                        printf("O jogador %s venceu o jogo!\n\n", quem);
+                        move[0] = 'q';
+                    }else if ((tabuleiro[0][0] && tabuleiro[2][0] && tabuleiro[4][0]) || (tabuleiro[0][2] && tabuleiro[2][2] && tabuleiro[4][2]) || (tabuleiro[0][4] && tabuleiro[2][4] && tabuleiro[4][4])== quem){
+                        system("clear");
+                        printf("TEMOS UM VENCENDOR!\n\n");
+                        printf("O jogador %s venceu o jogo!\n\n", quem);
+                        move[0] = 'q';
+                    }else if((tabuleiro[0][0] && tabuleiro[0][2] && tabuleiro[0][4]) || (tabuleiro[2][0] && tabuleiro[2][2] && tabuleiro[2][4]) || (tabuleiro[4][0] && tabuleiro[4][2] && tabuleiro[4][4])== quem){
+                        system("clear");
+                        printf("TEMOS UM VENCENDOR!\n\n");
+                        printf("O jogador %s venceu o jogo!\n\n", quem);
+                        move[0] = 'q';
+                    }
+                    ++turno;
+                    system("clear");
+                }
+            }
+            if (move[0] == 'q') {
+                break;
+            }
         }
         if (move[0] == 'q') {
             break;
